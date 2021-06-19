b4960f2c9b586c96b888ed9421870bb7238f410f
Hakan Hueriyet <hakan.hueriyet@justeattakeaway.com>
2021-06-19 22:02:07 +0200

    initial commit

--- server-modules/functions/src/services/storage.service.ts
+++
@@ -1,7 +0,0 @@
-import FirebaseController from "../controller/firebase.controller";
-
-const getStorageImage = () => {
-  return FirebaseController.bucket.file("/assets/images/logo.png").makePublic();
-};
-
-export { getStorageImage };
